2b27d1f84ddfcab1c3e2703fe7dda51734c07aff
Kajal Choudhary <108188712+kajalchoudhary1003@users.noreply.github.com>
2023-10-13 22:09:43 +0530

    frontend ui

--- extension/src/App.js
+++ extension/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import PlayCircleIcon from '@mui/icons-material/PlayCircle';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // parent-div
+    <div className='relative parent p-3 flex flex-col bg-cover bg-no-repeat bg-scroll backdrop-blur-md h-screen w-screen'>
+      <div className='flex flex-row justify-end'>
+      <h1 className='font-bold text-lg text-blue-base'>13 January, 2023</h1>
+      </div>
+
+{/* add task div */}
+<div className='flex flex-row rounded-md bg-base text-text-base cursor-pointer p-1 w-fit my-2 hover:translate-y-1'>
+  <h2 className='text-base text-text-base mr-2 font-semibold'>Add Task</h2>
+  <AddCircleIcon />
+
+</div>
+{/* play songs div */}
+<div className='flex flex-row rounded-md cursor-pointer p-1 w-fit text-text-base bg-dark-base my-2'>
+  <h2 className='font-semibold mr-2 text-base text-text-base'>Play Songs</h2>
+<PlayCircleIcon />
+</div>
+{/* quote div */}
+<div className='flex flex-row justify-center absolute inset-x-0 bottom-0'>
+<p className='animate-bounce p-1 border-2 border-blue-base text-dark-base w-fit rounded-md my-2 bg-text-base text-base'>Quote of the Day</p>
+</div>
+
     </div>
   );
 }
---
+++ extension/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+'base':'#454545',
+'dark-base':'#D1512D',
+'light-base':'#FFCB42',
+'blue-base':'#277BC0',
+'text-base':'#FFF6F4',
+      },
+    },
+  },
+  plugins: [],
+}
+
